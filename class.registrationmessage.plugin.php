9c68ba9c3797f033fb3575a345e8816603cededb
R-J <robin.jurinka@gmx.net>
2017-02-15 16:51:26 +0100

    Add functionality to also send a mail. Since new users get informed about PMs anyway, that was a complete waste of time...

--- class.registrationmessage.plugin.php
+++ class.registrationmessage.plugin.php
@@ -3,7 +3,7 @@
 $PluginInfo['registrationmessage'] = [
     'Name' => 'Registration Message',
     'Description' => 'Sends a configurable message to users immediately after registration.',
-    'Version' => '0.2',
+    'Version' => '0.3',
     'RequiredApplications' => ['Vanilla' => '2.2'],
     'MobileFriendly' => true,
     'SettingsUrl' => 'settings/registrationmessage',
@@ -14,20 +14,38 @@ $PluginInfo['registrationmessage'] = [
 ];
 
 class RegistrationMessagePlugin extends Gdn_Plugin {
-
     public function userModel_afterRegister_handler($sender, $args) {
+        $user = Gdn::userModel()->getID($args['UserID']);
+
+        // Send welcome mail.
+        $email = new Gdn_Email();
+        $email
+            ->setFormat('html')
+            ->subject(str_replace('%%NAME%%', $user->Name, c('RegistrationMessage.MailSubject')))
+            ->message(str_replace('%%NAME%%', $user->Name, c('RegistrationMessage.MailBody')))
+            ->to($user->Email);
+        try {
+            $email->send();
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+        // Send welcome PM.
         if (!c('EnabledApplications.Conversations')) {
             return;
         }
 
-        $name = val('Name', Gdn::userModel()->getID($args['UserID']));
-
-        (new ConversationModel())->save([
-            'Body' => str_replace('%%NAME%%', $name, c('RegistrationMessage.Message')),
+        $conversationMessage = [
+            'Body' => str_replace('%%NAME%%', $user->Name, c('RegistrationMessage.MessageBody')),
             'Format' => 'Html',
             'InsertUserID' => c('RegistrationMessage.User', Gdn::userModel()->getSystemUserID()),
             'RecipientUserID' => [$args['UserID']]
-        ], new ConversationMessageModel());
+        ];
+        if (c('Conversations.Subjects.Visible')) {
+            $conversationMessage['Subject'] = str_replace('%%NAME%%', $user->Name, c('RegistrationMessage.MessageSubject'));
+        }
+
+        (new ConversationModel())->save($conversationMessage, new ConversationMessageModel());
     }
 
 
@@ -37,9 +55,25 @@ class RegistrationMessagePlugin extends Gdn_Plugin {
 
         $conf = new ConfigurationModule($sender);
         $conf->initialize([
-            'RegistrationMessage.Message' => [
+            'RegistrationMessage.MailSubject' => [
                 'Control' => 'textbox',
-                'LabelCode' => 'Write a message to send to newly registered users.',
+                'LabelCode' => 'Subject of Welcome Mails.',
+                'Description' => 'HTML is allowed. You can use <code>%%NAME%%</code> as a placeholder for the user\'s name.'
+            ],
+            'RegistrationMessage.MailBody' => [
+                'Control' => 'textbox',
+                'LabelCode' => 'Write a message which will be mailed to newly registered users.',
+                'Description' => 'HTML is allowed. You can use <code>%%NAME%%</code> as a placeholder for the user\'s name.',
+                'Options' => ['MultiLine' => true]
+            ],
+            'RegistrationMessage.MessageSubject' => [
+                'Control' => 'textbox',
+                'LabelCode' => 'Subject of Welcome Messages.',
+                'Description' => 'This will only be used if you use subjects in conversation messages.'
+            ],
+            'RegistrationMessage.MessageBody' => [
+                'Control' => 'textbox',
+                'LabelCode' => 'Write a message which will be send as a PM to newly registered users.',
                 'Description' => 'HTML is allowed. You can use <code>%%NAME%%</code> as a placeholder for the user\'s name.',
                 'Options' => ['MultiLine' => true]
             ]
@@ -52,18 +86,18 @@ class RegistrationMessagePlugin extends Gdn_Plugin {
 
     public function base_getAppSettingsMenuItems_handler($sender, &$args) {
         $args['SideMenu']->addLink(
-          'Users',
-          'Registration Message',
-          'settings/registrationmessage',
-          'Garden.Settings.Manage'
+            'Users',
+            'Registration Message',
+            'settings/registrationmessage',
+            'Garden.Settings.Manage'
         );
     }
 
 
     public function setup() {
-        if (!c('RegistrationMessage.Message')) {
-            saveToConfig('RegistrationMessage.Message', 'Hi %%NAME%%, welcome to the community!');
-        }
+        touchConfig('RegistrationMessage.MailSubject', 'Welcome to '.c('Garden.Title'));
+        touchConfig('RegistrationMessage.MailBody', 'Hi %%NAME%%, welcome to the community!');
+        touchConfig('RegistrationMessage.MessageSubject', 'Welcome to '.c('Garden.Title'));
+        touchConfig('RegistrationMessage.MessageBody', 'Hi %%NAME%%, welcome to the community!');
     }
-
 }
